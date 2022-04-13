b5a3347a6c41e1e2f02092163c2d6479bc5794f3
Manali1892 <96368868+Manali1892@users.noreply.github.com>
2022-04-14 00:17:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Dashboard from "./component/Dashboard";
+import Sidebar from "./component/Sidebar";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <div id="wrapper">
+      <Sidebar></Sidebar>
+      <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <Navbar></Navbar>
+          <div className="container-fluid">
+            <Dashboard></Dashboard>
+          </div>
+        </div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+export default App;
